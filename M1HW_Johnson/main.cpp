966ebc6aca6ca61aa3f735288c3a234b93bb7e29
johnsonn6003 <46724850+johnsonn6003@users.noreply.github.com>
2019-01-17 13:56:27 -0500

    Add files via upload

---
+++ M1HW_Johnson/main.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M1HW1
+// N Johnson
+// 1/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // provide a brief introduction
+
+    cout << "Hello, my name is Nigel Johnson" << endl;
+    cout << "I'm in the associates in science program" << endl;
+    cout << "I have no programming background" << endl;
+    cout << "I like reading comics" << endl;
+    cout << "I have " << 1 << " pet" << endl;
+    cout << "testing\t1\t2\t3" <<endl;
+    return 0;
+}
